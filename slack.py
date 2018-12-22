1464e47dbefa7a574a9305c1697aeafe64301f9a
Shelley Cooper-White <hello@shelley.io>
2018-12-22 11:04:21 +1100

    add message sending to channel

--- slack.py
+++ slack.py
@@ -1,6 +1,20 @@
 from flask import Flask, request, jsonify, json
 
+import requests
+
 app = Flask(__name__)
+
+
+def send_message(text, channel):
+  '''Send a message of a particular type to a slack channel.'''
+  if text:
+    json_response = {'text': text, 'channel': channel}
+    headers = {
+      'Authorization': 'Bearer xoxb-509687114689-511356328919-sfElXzwnCkF8aMLLwJVpeCMs',
+      'Content-Type': 'application/json',
+    }
+    requests.post('https://slack.com/api/chat.postMessage', data=json.dumps(json_response), headers=headers)
+
 
 @app.route('/lol', methods=['GET', 'POST'])
 def lol_bot():
@@ -16,9 +30,15 @@ def event():
   print(request.values)
   print(request.get_json())
 
-  challenge = request.get_json().get('challenge')
+  payload = request.get_json()
+  # if payload:
+  #   event = payload.get('event')
+  #   if event.get('type') == 'message':
+  #     # todo: Send message back to user
 
-  return jsonify({'challenge': challenge})
+  send_message('Hello from bot!', 'DF08WMPR8')
+
+  return ('', 204)
 
 if __name__ == 'main':
   app.run()
